ccdb3230ae1cdc9936c95ce8c339f550e41fa45d
Lola Rol <lola.rol@hva.nl>
2020-09-03 15:19:38 +0200

    Add changes made before fixing issues

---
+++ Room/Assets/Scripts/RocketScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RocketScript : MonoBehaviour
+{
+    public GameObject player;
+
+    [SerializeField]
+    private GameObject rocketText;
+    [SerializeField]
+    private float distance;
+
+    private void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, this.transform.position);
+    }
+
+    private void OnMouseEnter()
+    {
+        rocketText.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        rocketText.SetActive(false);
+    }
+}
