461768e20ca8d1d7b430c7da8b991d2f6f175ee4
JEANYPC\Gebruiker <jeanydevries@hotmail.com>
2020-09-03 09:47:46 +0200

    Init

---
+++ Room/Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+
+    private Rigidbody M_rigidbody;
+
+    private void Start()
+    {
+        M_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            M_rigidbody.velocity = transform.forward * movementSpeed;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            M_rigidbody.velocity = -transform.forward * movementSpeed;
+        }
+    }
+}
---
+++ Room/Assets/Scripts/interactionScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactionScript : MonoBehaviour
+{
+    [SerializeField] private GameObject interactionUI;
+    [SerializeField] private Transform sittingPoint;
+
+    private bool isInField, isGoingToSit, isSitting;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && isInField && !isGoingToSit)
+        {
+            isGoingToSit = true;
+        }
+
+        if(isGoingToSit)
+        {
+            player.transform.position = sittingPoint.transform.position;
+            interactionUI.SetActive(false);
+
+            isGoingToSit = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            interactionUI.SetActive(true);
+            isInField = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        interactionUI.SetActive(false);
+        isInField = false;
+    }
+}
